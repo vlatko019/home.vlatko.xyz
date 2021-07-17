76299b064996bfa1f4bcec0717ae3095ffb7d085
Vlatko <vlatko1997@gmail.com>
2021-07-17 19:46:14 +0200

    Fixed deploy
    Now works recursively

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 ssh vlatko019@pi.hole "rm -rf /var/www/home.vlatko.xyz/*"
-scp dist\home-vlatko-xyz\* vlatko019@pi.hole:/var/www/home.vlatko.xyz/
+scp -r dist\home-vlatko-xyz\* vlatko019@pi.hole:/var/www/home.vlatko.xyz/
 echo deployed!
